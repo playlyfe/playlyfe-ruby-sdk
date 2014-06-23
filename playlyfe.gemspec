9eba8dc24f8c5ae0bd9ead674c3eb85a829402ff
Peter John <peter@playlyfe.com>
2014-06-23 11:42:34 +0530

    fix: backport oauth2.0 client to older rails version and add rails test

--- playlyfe.gemspec
+++ playlyfe.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = "playlyfe"
-  s.version     = '0.1.0'
+  s.version     = '0.3.0'
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.rubygems_version = %q{1.6.2}
   s.license     = 'ApacheV2'
@@ -12,6 +12,8 @@ Gem::Specification.new do |s|
   s.test_files = ["test/test.rb"]
   s.homepage    = 'https://github.com/pyros2097/playlyfe-ruby-sdk'
   s.require_paths = ["lib"]
-  s.add_runtime_dependency "oauth2", ">= 0.9.4"
-  s.add_runtime_dependency "json", ">= 1.8.1"
+  s.add_runtime_dependency 'faraday', '0.8'
+  s.add_runtime_dependency 'jwt', '1.0'
+  s.add_runtime_dependency 'json', '1.8.1'
 end
+
--- test/test.rb
+++ test/test.rb
@@ -4,27 +4,28 @@ require 'playlyfe'
 class PlaylyfeTest < Test::Unit::TestCase
 
   def test_start
-    Playlyfe.start(
-      client_id: "MTQ5MDFiODAtYzYzNS00OTMyLTg4NDktYjliNzE0ZmZiZDBl",
-      client_secret: "ZjI0YmNlNzgtNzU2ZC00NWU0LWIwZTItNjg1YjU4MTljNDRlODA4MDkyYzAtZTg3ZS0xMWUzLWE5ZTMtMzViZDM4NGNiMjQw"
+    Playlyfe.init(
+      client_id: "NjZlZWFiMWEtN2ZiOC00YmVmLTk2YWMtNDEyYTNmMjQ5NDBl",
+      client_secret: "NGRhMWJlYzUtODJiNS00ZTdkLTgzNTctMTQyMGEzNTljN2MwMTU2MGM3NTAtZjZiZS0xMWUzLTgxZjYtNmZhMGYyMzRkZGU3"
     )
-    puts Playlyfe.token
+    puts Playlyfe.token.to_hash
 
     players = Playlyfe.get(url: '/players')
     assert_not_nil players["data"]
     assert_not_nil players["data"][0]
 
-    player = Playlyfe.get(url: '/player', player: 'goku')
-    assert_equal player["id"], "goku"
-    assert_equal player["alias"], "goku"
+    player_id = 'student1'
+    player = Playlyfe.get(url: '/player', player: player_id)
+    assert_equal player["id"], "student1"
+    assert_equal player["alias"], "Student1"
     assert_equal player["enabled"], true
 
     no_player = Playlyfe.get(url: '/player')
     assert_nil no_player
 
-    Playlyfe.get(url: "/definitions/processes", player: 'goku')
-    Playlyfe.get(url: "/definitions/teams", player: 'goku')
-    Playlyfe.get(url: "/processes", player: 'goku')
-    Playlyfe.get(url: "/teams", player: 'goku')
+    Playlyfe.get(url: "/definitions/processes", player: player_id)
+    Playlyfe.get(url: "/definitions/teams", player: player_id)
+    Playlyfe.get(url: "/processes", player: player_id)
+    Playlyfe.get(url: "/teams", player: player_id)
   end
 end
